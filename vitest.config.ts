66b7aa188eff37c41fa4f92d46daab4814c6fa9f
ChingChingKao <101656913+ChingChingKao@users.noreply.github.com>
2026-05-16 14:14:22 +0000

    fix: format vitest config and workflow file

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,11 +1,11 @@
-import { defineConfig } from 'vitest/config'
+import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
     exclude: ['dist/**', 'node_modules/**'],
     reporters: ['default', 'junit'],
     outputFile: {
-      junit: './test-results/junit.xml',
-    },
-  },
-})
+      junit: './test-results/junit.xml'
+    }
+  }
+});
