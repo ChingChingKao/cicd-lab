8b1144534e61d80b56950fefb56dea0835853ff9
ChingChingKao <101656913+ChingChingKao@users.noreply.github.com>
2026-05-16 14:11:28 +0000

    fix: update vitest config to generate junit report

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,10 +1,6 @@
-import type { TestUserConfig, ViteUserConfig } from 'vitest/config'
+import { defineConfig } from 'vitest/config'
 
-type VitestConfig = ViteUserConfig & {
-  test?: TestUserConfig
-}
-
-const config: VitestConfig = {
+export default defineConfig({
   test: {
     exclude: ['dist/**', 'node_modules/**'],
     reporters: ['default', 'junit'],
@@ -12,6 +8,4 @@ const config: VitestConfig = {
       junit: './test-results/junit.xml',
     },
   },
-}
-
-export default config
+})
